fff007d2e31d701418d68a3f897218679747b45d
Arnav Dixit <arnavdixit127@gmail.com>
2020-07-27 19:32:27 +0530

    Updated about.go

--- about.go
+++ about.go
@@ -8,8 +8,8 @@ func printabout() *cobra.Command {
 	return &cobra.Command{
 		Use:   "about",
 		Short: "Print details about the author",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			cmd.Println("This software is developed by Arnav Dixit \nhttps://github.com/arnav127")
+		RunE: func(app *cobra.Command, args []string) error {
+			app.Println("This software is developed by Arnav Dixit \nhttps://github.com/arnav127")
 			return nil
 		},
 	}
