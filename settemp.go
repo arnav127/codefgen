e1a41889149bfea65eed02b5e61a9fb6ff4be601
Arnav Dixit <arnavdixit127@gmail.com>
2020-07-29 21:51:14 +0530

    Added option to change template

---
+++ settemp.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"os/user"
+
+	"github.com/spf13/cobra"
+)
+
+func settemp() *cobra.Command {
+	return &cobra.Command{
+		Use:   "stemp",
+		Short: "Set template file",
+		RunE: func(app *cobra.Command, args []string) error {
+			newtemplate()
+			return nil
+		},
+	}
+}
+
+func newtemplate() {
+	fmt.Println("Enter new file path for template (absolute path): ")
+	var path string
+	fmt.Scanln(&path)
+
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		fmt.Println("The specified file does not exist. Exiting")
+		return
+	}
+	usr, err := user.Current()
+	check(err)
+
+	filename := usr.HomeDir + "/.config/codef-gen/config"
+	script := usr.HomeDir + "/.config/codef-gen/stemp.sh"
+	cmd := exec.Command("/bin/sh", script, path, filename)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	errr := cmd.Run()
+	if errr != nil {
+		fmt.Println(errr)
+	}
+	// fmt.Println("Changed " + path + " to " + filename)
+}
